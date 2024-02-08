fe9fcab0af8eb675269da94e78c701220d0cf483
Paramvir Das <paramvir2307@gmail.com>
2024-02-08 20:32:42 +0100

    Finish Todo creation

--- life-track-hub/src/main.rs
+++ life-track-hub/src/main.rs
@@ -1,6 +1,12 @@
-use time::OffsetDateTime;
+#[macro_use]
+extern crate rocket;
 
-fn main() {
-    let now = OffsetDateTime::now_local().unwrap();
-    println!("{now}");
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
--- life-track-hub/src/structures/todo.rs
+++ life-track-hub/src/structures/todo.rs
@@ -5,7 +5,8 @@ pub struct Todo {
     description: String,
     date_created: OffsetDateTime,
     date_modified: OffsetDateTime,
-    date_completed: OffsetDateTime,
+    date_deadline: Option<OffsetDateTime>,
+    date_completed: Option<OffsetDateTime>,
     is_completed: bool,
 }
 
@@ -23,8 +24,11 @@ impl Todo {
     pub fn get_date_modified(&self) -> &OffsetDateTime {
         return &self.date_modified;
     }
-    pub fn get_date_completed(&self) -> &OffsetDateTime {
+    pub fn get_date_completed(&self) -> &Option<OffsetDateTime> {
         return &self.date_completed;
+    }
+    pub fn get_date_deadline(&self) -> &Option<OffsetDateTime> {
+        return &self.date_deadline;
     }
     pub fn is_completed(&self) -> bool {
         return self.is_completed;
@@ -33,24 +37,48 @@ impl Todo {
     // setters
     pub fn set_title(&mut self, title: &str) {
         self.title = String::from(title);
-        self.date_modified = OffsetDateTime::now_local().unwrap();
+        self.update_date_modified();
     }
     pub fn set_description(&mut self, description: &str) {
         self.description = String::from(description);
-        self.date_modified = OffsetDateTime::now_local().unwrap();
+        self.update_date_modified();
     }
-    pub fn set_date_completed(&mut self, date_completed: &OffsetDateTime) {
-        self.date_completed = *date_completed;
-        self.date_modified = OffsetDateTime::now_local().unwrap();
+    pub fn set_date_completed(&mut self, date_completed: Option<OffsetDateTime>) {
+        self.date_completed = date_completed;
+    }
+    pub fn set_date_deadline(&mut self, date_deadline: Option<OffsetDateTime>) {
+        self.date_completed = date_deadline;
     }
     pub fn set_completed(&mut self, is_completed: bool) {
         self.is_completed = is_completed;
-        self.date_modified = OffsetDateTime::now_local().unwrap();
+        self.update_date_modified();
     }
 
     // additional functions
+    pub fn new(title: &str) -> Self {
+        Todo {
+            title: String::from(title),
+            description: String::from(""),
+            date_created: Self::get_current_local_time(),
+            date_modified: Self::get_current_local_time(),
+            date_deadline: None,
+            date_completed: None,
+            is_completed: false,
+        }
+    }
     pub fn toggle_is_completed(&mut self) {
         self.is_completed = !self.is_completed;
-        self.date_modified = OffsetDateTime::now_local().unwrap();
+        self.update_date_modified();
+    }
+
+    // private functions
+    fn update_date_modified(&mut self) {
+        self.date_modified = Self::get_current_local_time();
+    }
+
+    fn get_current_local_time() -> OffsetDateTime {
+        OffsetDateTime::now_local().unwrap_or_else(|err| {
+            panic!("Error getting current local time: {:?}", err);
+        })
     }
 }
