50860f5d93bab06bffeaf41d5fb790edc5581aa1
Paramvir Das <paramvir2307@gmail.com>
2024-06-13 18:21:39 +0200

    - added documentation - prepare for v0.0.1 release

--- aayojak-lib/src/lib.rs
+++ aayojak-lib/src/lib.rs
@@ -1,2 +1,7 @@
+//! aayojak-lib
+//!
+//! Aayojak is a tool which should help organize day to day tasks. This crate is a library crate and contains the basic data structures used by Aayojak.
+
 pub mod structures;
 pub mod traits;
+pub use self::structures::todo::Todo;
